daeb5af720ee6f8faff325caf9be1af492746909
Francesca Hamm <francesca.c.hamm@gmail.com>
2021-11-13 00:56:52 -0700

    commit tree 12f2fbd21ee2794c01d9e5c68990b2c44f4bae1b

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(namesArray, string) {
+    return namesArray.filter(function(el) {
+        return el.toLowerCase().indexOf(string.toLowerCase()) !== -1
+})
+}
+console.log(findMatching(drivers, string))
+
+// 
+
+function fuzzyMatch(namesArray, letter) {
+    return namesArray.filter((namesArray) => namesArray.startsWith(letter))
+}
+ 
+//
+
+function matchName(driverObj, string) {
+    return driverObj.filter((driverObj) => driverObj.name === string)
+}
+
